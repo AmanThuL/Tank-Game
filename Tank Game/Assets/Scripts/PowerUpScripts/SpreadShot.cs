4ee9156d187d1f0919ebc0b0923e6f7ee9e6cdcc
gavmat8 <gavmat8@gmail.com>
2020-04-09 21:31:56 -0400

    SpreadShot Powerup Created

---
+++ Tank Game/Assets/Scripts/PowerUpScripts/SpreadShot.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpreadShot : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Blue_Tank(Clone)" || collision.gameObject.name == "Red_Tank(Clone)")
+        {
+            //Infinite ammo for limited time
+            collision.gameObject.GetComponent<TankControls>().SpreadShot();
+            GameStats.Instance.changeTime = Time.time;
+            Destroy(this.gameObject);
+            GameStats.Instance.powerUpSpawned = false;
+        }
+    }
+}
