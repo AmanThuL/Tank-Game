89fa5aff936609e0bef50e291ebdfaad06fef929
Gavin <gmd3659@rit.edu>
2020-04-24 16:12:29 -0400

    Game Mode Options added and implemented but causing stack overflow

---
+++ Tank Game/Assets/Scripts/GameModeSettings.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameModeSettings : MonoBehaviour
+{
+
+    public Toggle flagToggle;
+    public Toggle stockToggle;
+    public Toggle timeToggle;
+
+    void Start()
+    {
+        switch (GameStats.Instance.mode)
+        {
+            case GameMode.Flag:
+
+                flagToggle.isOn = true;
+                stockToggle.isOn = false;
+                timeToggle.isOn = false;
+
+                break;
+            case GameMode.Time:
+
+                flagToggle.isOn = false;
+                stockToggle.isOn = false;
+                timeToggle.isOn = true;
+
+                break;
+            case GameMode.Lives:
+
+                flagToggle.isOn = false;
+                stockToggle.isOn = true;
+                timeToggle.isOn = false;
+
+                break;
+            default:
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void FlagModeOn()
+    {
+        if (GameStats.Instance.mode == GameMode.Flag)
+        {
+            flagToggle.isOn = true;
+        }
+        else
+        {
+            GameStats.Instance.mode = GameMode.Flag;
+            flagToggle.isOn = true;
+            stockToggle.isOn = false;
+            timeToggle.isOn = false;
+        }
+    }
+
+    public void TimeModeOn()
+    {
+        if (GameStats.Instance.mode == GameMode.Time)
+        {
+            timeToggle.isOn = true;
+        }
+        else
+        {
+            GameStats.Instance.mode = GameMode.Time;
+            flagToggle.isOn = false;
+            stockToggle.isOn = false;
+            timeToggle.isOn = true;
+        }
+    }
+
+    public void StockModeOn()
+    {
+        if (GameStats.Instance.mode == GameMode.Lives)
+        {
+            stockToggle.isOn = true;
+        }
+        else
+        {
+            GameStats.Instance.mode = GameMode.Lives;
+            flagToggle.isOn = false;
+            stockToggle.isOn = true;
+            timeToggle.isOn = false;
+        }
+    }
+
+}
